f29e8b8de812272963276c3e456695261aae9800
nicasop <nicolas_villita21@hotmail.es>
2022-05-29 22:22:21 -0500

    graficas

---
+++ app/distancias.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from . import nlp
+from . import jaccard
+from . import tdIdf as tdf
+from . import cosenoVectorial as cosV
+
+def matrizDistanciaPonderada(matriz1,matriz2,matriz3,ponderacines):
+    matrizDistancias = np.zeros((len(matriz1),len(matriz1)))
+    i = j = 0
+    while True:
+        matrizDistancias[i][j] = matriz1[i][j]*ponderacines[0] + matriz2[i][j]*ponderacines[1] + matriz3[i][j]*ponderacines[2]
+        j += 1
+        if j ==  len(matriz1):
+            j = 0
+            i += 1
+        if i == len(matriz1):
+            break
+    return matrizDistancias
+
+def matricesDistancia (datos):
+    titulos = datos['titulo'].tolist()
+    keywords = datos['keywords'].tolist()
+    abstracts = datos['abstract'].tolist()
+    ##NLP
+    titulosL = nlp.limpiarDocumento(titulos,'en')
+    keywordsL = nlp.limpiarDocumento(keywords,'en')
+    abstractsL = nlp.limpiarDocumento(abstracts,'en')
+    ##JACCARD
+    #TITULOS
+    matrizTit = jaccard.matrizJaccard(titulosL)
+    #KEYWORDS
+    matrizKey = jaccard.matrizJaccard(keywordsL)
+    ##FULL INVERTED INDEX -- TF-IDF -- COSENO VECTORIAL
+    #FULL INVERTED INDEX
+    diccionario={'tokens':[],'ocurrencias':[]}
+    diccionario['tokens']= nlp.indexacionToken(abstractsL)
+    diccionario['ocurrencias'] = nlp.ocurrencias(diccionario['tokens'],abstractsL)
+    #TF-IDF
+    matriz = tdf.bagWords(diccionario,abstractsL)
+    wtf = tdf.matrizPTF(matriz)
+    dF = tdf.documentF(wtf)
+    idf = tdf.IDF(dF,len(abstractsL))
+    tf_idf = tdf.TFIDF(wtf,idf)
+    #Coseno Vectorial
+    matrizN = cosV.matrizNormal(tf_idf)
+    matrizAbs = cosV.matrizDistacias(matrizN)
+    ##MATRIZ DE DISTACIAS
+    ponderacion = [0.5,0.3,0.2]
+    Distancias = matrizDistanciaPonderada(matrizAbs,matrizKey,matrizTit,ponderacion)
+    return matrizTit,matrizKey,matrizAbs,Distancias
