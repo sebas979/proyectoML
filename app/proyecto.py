9ad4581fd383f9eb6b70952cd9886cf60ae942e2
nicasop <nicolas_villita21@hotmail.es>
2022-05-30 20:06:42 -0500

    cambios matriz de papers y filtro heatmap

--- app/proyecto.py
+++ app/proyecto.py
@@ -61,12 +61,108 @@ def matriz():
     matriz = list(zip(datosfiltrados['titulo'].tolist(),datosfiltrados['keywords'].tolist(),datosfiltrados['abstract'].tolist()))
     return render_template('paginas/matriz.html',filas=len(matriz),matriz=matriz)
 
-@bp.route('/graficos', methods=['GET'])
+@bp.route('/graficos', methods=['GET','POST'])
 def graficos():
-    codigo_img_total = graficoMapaCalor(M)
-    codigo_img_titulos = graficoMapaCalor(mT)
-    codigo_img_keywords = graficoMapaCalor(mK)
-    codigo_img_abstracts = graficoMapaCalor(mA)
+    if request.method == 'POST':
+        paper = request.form.get('paper',type=int)
+        tipo = request.form.get('tipo')
+        print(paper)
+        if tipo == 'exactas':
+            if paper == 1:
+                codigo_img_total = graficoMapaCalor(M[0:15,0:15])
+                codigo_img_titulos = graficoMapaCalor(mT[0:15,0:15])
+                codigo_img_keywords = graficoMapaCalor(mK[0:15,0:15])
+                codigo_img_abstracts = graficoMapaCalor(mA[0:15,0:15])
+            elif paper == 2:
+                codigo_img_total = graficoMapaCalor(M[16:31,16:31])
+                codigo_img_titulos = graficoMapaCalor(mT[16:31,16:31])
+                codigo_img_keywords = graficoMapaCalor(mK[16:31,16:31])
+                codigo_img_abstracts = graficoMapaCalor(mA[16:31,16:31])
+            elif paper == 3:
+                codigo_img_total = graficoMapaCalor(M[32:47,32:47])
+                codigo_img_titulos = graficoMapaCalor(mT[32:47,32:47])
+                codigo_img_keywords = graficoMapaCalor(mK[32:47,32:47])
+                codigo_img_abstracts = graficoMapaCalor(mA[32:47,32:47])
+            else:
+                codigo_img_total = graficoMapaCalor(M[0:47,0:47])
+                codigo_img_titulos = graficoMapaCalor(mT[0:47,0:47])
+                codigo_img_keywords = graficoMapaCalor(mK[0:47,0:47])
+                codigo_img_abstracts = graficoMapaCalor(mA[0:47,0:47])
+
+        elif tipo == 'medi':
+            if paper == 1:
+                codigo_img_total = graficoMapaCalor(M[48:63,48:63])
+                codigo_img_titulos = graficoMapaCalor(mT[48:63,48:63])
+                codigo_img_keywords = graficoMapaCalor(mK[48:63,48:63])
+                codigo_img_abstracts = graficoMapaCalor(mA[48:63,48:63])
+            elif paper == 2:
+                codigo_img_total = graficoMapaCalor(M[64:79,64:79])
+                codigo_img_titulos = graficoMapaCalor(mT[64:79,64:79])
+                codigo_img_keywords = graficoMapaCalor(mK[64:79,64:79])
+                codigo_img_abstracts = graficoMapaCalor(mA[64:79,64:79])
+            elif paper == 3:
+                codigo_img_total = graficoMapaCalor(M[80:95,80:95])
+                codigo_img_titulos = graficoMapaCalor(mT[80:95,80:95])
+                codigo_img_keywords = graficoMapaCalor(mK[80:95,80:95])
+                codigo_img_abstracts = graficoMapaCalor(mA[80:95,80:95])
+            else:
+                codigo_img_total = graficoMapaCalor(M[48:95,48:95])
+                codigo_img_titulos = graficoMapaCalor(mT[48:95,48:95])
+                codigo_img_keywords = graficoMapaCalor(mK[48:95,48:95])
+                codigo_img_abstracts = graficoMapaCalor(mA[48:95,48:95])
+        elif tipo == 'compu':
+            if paper == 1:
+                codigo_img_total = graficoMapaCalor(M[96:111,96:111])
+                codigo_img_titulos = graficoMapaCalor(mT[96:111,96:111])
+                codigo_img_keywords = graficoMapaCalor(mK[96:111,96:111])
+                codigo_img_abstracts = graficoMapaCalor(mA[96:111,96:111])
+            elif paper == 2:
+                codigo_img_total = graficoMapaCalor(M[112:127,112:127])
+                codigo_img_titulos = graficoMapaCalor(mT[112:127,112:127])
+                codigo_img_keywords = graficoMapaCalor(mK[112:127,112:127])
+                codigo_img_abstracts = graficoMapaCalor(mA[112:127,112:127])
+            elif paper == 3:
+                codigo_img_total = graficoMapaCalor(M[128:143,128:143])
+                codigo_img_titulos = graficoMapaCalor(mT[128:143,128:143])
+                codigo_img_keywords = graficoMapaCalor(mK[128:143,128:143])
+                codigo_img_abstracts = graficoMapaCalor(mA[128:143,128:143])
+            else:
+                codigo_img_total = graficoMapaCalor(M[96:143,96:143])
+                codigo_img_titulos = graficoMapaCalor(mT[96:143,96:143])
+                codigo_img_keywords = graficoMapaCalor(mK[96:143,96:143])
+                codigo_img_abstracts = graficoMapaCalor(mA[96:143,96:143])
+        elif tipo == 'sociales':
+            if paper == 1:
+                codigo_img_total = graficoMapaCalor(M[144:159,144:159])
+                codigo_img_titulos = graficoMapaCalor(mT[144:159,144:159])
+                codigo_img_keywords = graficoMapaCalor(mK[144:159,144:159])
+                codigo_img_abstracts = graficoMapaCalor(mA[144:159,144:159])
+            elif paper == 2:
+                codigo_img_total = graficoMapaCalor(M[160:175,160:175])
+                codigo_img_titulos = graficoMapaCalor(mT[160:175,160:175])
+                codigo_img_keywords = graficoMapaCalor(mK[160:175,160:175])
+                codigo_img_abstracts = graficoMapaCalor(mA[160:175,160:175])
+            elif paper == 3:
+                codigo_img_total = graficoMapaCalor(M[176:191,176:191])
+                codigo_img_titulos = graficoMapaCalor(mT[176:191,176:191])
+                codigo_img_keywords = graficoMapaCalor(mK[176:191,176:191])
+                codigo_img_abstracts = graficoMapaCalor(mA[176:191,176:191])
+            else:
+                codigo_img_total = graficoMapaCalor(M[144:191,144:191])
+                codigo_img_titulos = graficoMapaCalor(mT[144:191,144:191])
+                codigo_img_keywords = graficoMapaCalor(mK[144:191,144:191])
+                codigo_img_abstracts = graficoMapaCalor(mA[144:191,144:191])
+        else:
+            codigo_img_total = graficoMapaCalor(M)
+            codigo_img_titulos = graficoMapaCalor(mT)
+            codigo_img_keywords = graficoMapaCalor(mK)
+            codigo_img_abstracts = graficoMapaCalor(mA)
+    if request.method == 'GET':
+        codigo_img_total = graficoMapaCalor(M)
+        codigo_img_titulos = graficoMapaCalor(mT)
+        codigo_img_keywords = graficoMapaCalor(mK)
+        codigo_img_abstracts = graficoMapaCalor(mA)
+
     return render_template('paginas/graficas.html',imagen={
         'todo':codigo_img_total,
         'titulo':codigo_img_titulos,
